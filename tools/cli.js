f9abdf314fe5e11ed846206248be8d820c61cc7f
David Guenther <davidbguenther@gmail.com>
2017-03-06 02:02:59 -0600

    Improved command interpretation

--- tools/cli.js
+++ tools/cli.js
@@ -2,12 +2,18 @@ var exports = module.exports = {};
 
 //parses commands
 exports.parse = function(socket, io) {
-	return function(msg){ //TODO(torchhound) var command = jsonOut.command.split(' ');
+	return function(msg){
 		var jsonOut = JSON.parse(msg);
-		if(jsonOut.command.slice(0, 2) == 't ') {
-			msg = JSON.stringify({"name":jsonOut.name, "command":jsonOut.command.slice(2)});
-    		io.emit('chat', msg);
-		} else {
+		//commandSplit is each separate word of the command, which we will use to determine what actions the player wants to take.
+		//Note that jsonOut.command has not been modified as we wish to preserve the exact spacing of the original message.
+		var commandSplit = jsonOut.command.split(' ');
+		//If command is "Local Chat"
+		if(commandSplit[0] == 't' || commandSplit[0] == 'say') {
+			msg = JSON.stringify({"name":jsonOut.name+" says", "command":'\"'+jsonOut.command.substr(jsonOut.command.indexOf(" ") + 1)+'\"'});
+    		io.emit('chat', msg);	
+		}
+		//If command is not a valid command
+		else {
 			msg = JSON.stringify({"command":"Invalid Command: "+jsonOut.command});
 			socket.emit('log', msg);
 		};
