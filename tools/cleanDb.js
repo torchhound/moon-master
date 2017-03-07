9498739d71d84ac90800ead166e9f0ea8bbe854f
Joseph E.S. Cieslik <unknownmasayoshi@gmail.com>
2017-03-07 01:31:16 -0500

    Added cleanDb.js, added termination to initDb.js

---
+++ tools/cleanDb.js
@@ -0,0 +1,19 @@
+const MongoClient = require('mongodb').MongoClient;
+
+var env = 'development';
+var config = require('../config')[env];
+
+MongoClient.connect(config.database.host, (error, database) => {
+  	if(error) {
+  		return console.log(error);
+ 	} else {
+  		console.log('initDb DB Connection Successful!');
+  		database.collection('rooms').drop();
+  		database.collection('players').drop();
+  		console.log('Dropped collections');
+  		database.createCollection('players');
+  		database.createCollection('rooms');
+  		console.log('Created collections');
+  		break;
+  	}
+});
