f4510e1221012b59c56deef2adf9711daebe8f23
Joseph E.S. Cieslik <unknownmasayoshi@gmail.com>
2017-03-14 00:38:41 -0400

    Moved move to a function in playerTools

--- tools/playerTools.js
+++ tools/playerTools.js
@@ -1,3 +1,7 @@
+const fs = require('fs');
+
+var exports = module.exports = {};
+
 //Takes in a skill, the base chance the check will succeed, the chance increase (in percentile points) per rank in the skill, and the exp awarded for trying and failing. 
 //returns "" on a success, or information about the failure if you failed.
 exports.skillCheck = function(player, skill, successBase, successSkillMod, exp) {
@@ -6,7 +10,7 @@ exports.skillCheck = function(player, skill, successBase, successSkillMod, exp) 
 		return "";
 	} else {
 		return "Failure! (" + exports.skillIncrease(player, skill, exp) + ")"; 
-	}
+	};
 };
 	
 //Takes in a skill, adds exp to it, checks for rank increase(s), returns a string with information if you want to show it to the player.
@@ -19,8 +23,7 @@ exports.skillIncrease = function(player, skill, exp) {
 		player.skills[skill].rank++;
 	};
 	output = "You gained " + exp + " " + player.skills[skill].name + " EXP!"
-	if (ranksGained > 0)
-		output += " Your rank increased by " + ranksGained + "!";
+	if (ranksGained > 0) output += " Your rank increased by " + ranksGained + "!";
 	return output;
 };
 
@@ -28,4 +31,142 @@ exports.skillIncrease = function(player, skill, exp) {
 //The rank put in should be the CURRENT rank of the skill, not the one you're trying to reach next.
 exports.expNeeded = function(rank) {
 	return Math.round(Math.pow(rank, 1.1) * 100)
-}
+};
+
+exports.move = function(direction, players, jsonOut, mapOut, socket, mapFile) {
+	var position;
+	players.forEach(function(result, index) {
+		var playerOut = JSON.parse(result);
+		if(playerOut.name === jsonOut.name.toLowerCase()) {
+			position = playerOut.position;
+		};
+	});
+	console.log('start position: '+position);
+	console.log('start map: '+mapOut.map[0][0]);
+	var p1 = position[0], 
+		p2 = position[1];
+	var oldRoomOut = JSON.parse(mapOut.map[p1][p2]);
+	switch(direction) {
+		case 'n':
+		case 'north':
+			if(position[0] === 0) { 
+				socket.emit('log', JSON.stringify({"command":"Cannot go any farther north"}));
+				return false; 
+			};
+			break;
+		case 's':
+		case 'south':
+			var pTest = position[0] + 1;
+			if(pTest > mapOut.map.length - 1) { 
+				socket.emit('log', JSON.stringify({"command":"Cannot go any farther south"}));
+				return false;
+			};
+			break;
+		case 'e':
+		case 'east':
+			var pTest = position[1] + 1;
+			if(pTest  > mapOut.map.length - 1) {
+				socket.emit('log', JSON.stringify({"command":"Cannot go any farther east"}));
+				return false;
+			};
+			break;
+		case 'w':
+		case 'west':
+			if(position[1] === 0) {
+				socket.emit('log', JSON.stringify({"command":"Cannot go any farther west"}));
+				return false; 
+			};
+			break;
+		default:
+			socket.emit('log', JSON.stringify({'command':'An error occured in your movement'}));
+	};
+	for(var y in oldRoomOut.players) {
+		if(jsonOut.name.toLowerCase() === oldRoomOut.players[y]) {
+			oldRoomOut.players.splice(y, 1);
+			mapOut.map[p1][p2] = JSON.stringify(oldRoomOut);
+			fs.writeFileSync(mapFile, JSON.stringify(mapOut));
+		};
+	};
+	switch(direction) {
+		case 'n':
+		case 'north':
+			position[0]--; 
+			break;
+		case 's':
+		case 'south':
+			position[0]++;
+			break;
+		case 'e':
+		case 'east':
+			position[1]++;
+			break;
+		case 'w':
+		case 'west':
+			position[1]--;
+			break;
+		default:
+			socket.emit('log', JSON.stringify({'command':'An error occured in your movement'}));
+	};
+	p1 = position[0], 
+	p2 = position[1];
+ 	var newRoomOut = JSON.parse(mapOut.map[p1][p2]);
+	if(newRoomOut.players.indexOf(jsonOut.name.toLowerCase()) === -1) {
+		newRoomOut.players.push(jsonOut.name.toLowerCase());
+		mapOut.map[p1][p2] = JSON.stringify(newRoomOut);
+		fs.writeFileSync(mapFile, JSON.stringify(mapOut));
+		switch(direction) {
+			case 'n':
+			case 'north': 
+				socket.emit('log', JSON.stringify({"command":"You moved north"}));
+				break;
+			case 's':
+			case 'south':
+				socket.emit('log', JSON.stringify({"command":"You moved south"}));
+				break;
+			case 'e':
+			case 'east':
+				socket.emit('log', JSON.stringify({"command":"You moved east"}));
+				break;
+			case 'w':
+			case 'west':
+				socket.emit('log', JSON.stringify({"command":"You moved west"}));
+				break;
+			default:
+				socket.emit('log', JSON.stringify({'command':'An error occured in your movement'}));
+		};
+	} else {
+		switch(direction) {
+		case 'n':
+		case 'north': 
+			console.log('move north: Player already exists in room');
+			socket.emit('log', JSON.stringify({'command':'move north: server error'})); 
+			break;
+		case 's':
+		case 'south':
+			console.log('move south: Player already exists in room');
+			socket.emit('log', JSON.stringify({'command':'move south: server error'}));
+			break;
+		case 'e':
+		case 'east':
+			console.log('move east: Player already exists in room');
+			socket.emit('log', JSON.stringify({'command':'move east: server error'}));
+			break;
+		case 'w':
+		case 'west':
+			console.log('move west: Player already exists in room');
+			socket.emit('log', JSON.stringify({'command':'move west: server error'}));
+			break;
+		default:
+			socket.emit('log', JSON.stringify({'command':'An error occured in your movement'}));
+		};
+	};				
+	players.forEach(function(result, index) {
+		var playerOut = JSON.parse(result);
+		if(playerOut.name === jsonOut.name.toLowerCase()) {
+			playerOut.position = position;
+			players[index] = JSON.stringify(playerOut);
+		};
+	});
+	console.log('end position: '+position);
+	console.log('end map: '+mapOut.map);
+};
